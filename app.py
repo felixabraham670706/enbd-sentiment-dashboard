851a21b2fff6c8fbf8c253fd9adadca542dfd3a2
felixabraham670706 <felixabraham670706@gmail.com>
2026-03-11 01:35:16 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from pipeline import run_pipeline
+from streamlit_autorefresh import st_autorefresh
+
+st.title("Emirates NBD Reddit Sentiment Dashboard")
+
+# refresh every 5 minutes
+st_autorefresh(interval=300000)
+
+df = run_pipeline()
+
+positive = df[df["sentiment"]=="Positive"]
+neutral = df[df["sentiment"]=="Neutral"]
+negative = df[df["sentiment"]=="Negative"]
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.subheader("Positive Comments")
+    for c in positive["comment"]:
+        st.success(c)
+
+with col2:
+    st.subheader("Neutral Comments")
+    for c in neutral["comment"]:
+        st.warning(c)
+
+with col3:
+    st.subheader("Negative Comments")
+    for c in negative["comment"]:
+        st.error(c)
